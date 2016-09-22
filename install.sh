7931913f762c5b7a94d3f423bf835dbf94938d3a
Overly <timothy.overly@gmail.com>
2016-09-22 13:05:09 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ mkdir spida
 cd spida
 wget https://raw.githubusercontent.com/spidasoftware/opentileserver/master/opentileserver.sh
 vi opentileserver.sh
-./opentileserver ssl bright http://download.geofabrik.de/north-america/us/ohio-latest.osm.pbf 
+chmod ./opentileserver.sh
+./opentileserver.sh ssl bright http://download.geofabrik.de/north-america/us/ohio-latest.osm.pbf 
