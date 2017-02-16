97e3f63f22716e3f2a444ca90d06589d5d94c804
David Ghedini <david@davidghedini.com>
2017-02-17 01:30:52 +0200

    Bug Fixes
    Fix cache calculation and added osmosis and leaflet update

--- reload-opentileserver.sh
+++ reload-opentileserver.sh
@@ -1,11 +1,8 @@
 #!/bin/bash -e
 #Version: 0.3
-#Description: Reload OSM data to server built with opentileserver.sh
+#Description: Reload OSM data
 #Note: This script assumes all external commands
 #	(pg_config, osm2pgsl, ...) are installed.
-#Usage: reload-opentileserver.sh [-add] [pbf_url]
-# To run in non-Latin language uncomment below
-#export LC_ALL=C
  
 PBF_ADD='no'
 if [ "${1}" == '-add' ]; then
@@ -13,7 +10,7 @@ if [ "${1}" == '-add' ]; then
 	shift 1;
 fi
  
-PBF_URL="${1}";	#Example: http://download.geofabrik.de/europe/germany-latest.osm.pbf
+PBF_URL="${1}";	#http://download.geofabrik.de/europe/andorra-latest.osm.pbf
 OSM_USER='tile'
 OSM_DB='gis'
  
@@ -22,10 +19,8 @@ if [ -z "${PBF_URL}" ]; then
 	echo "Usage: $0 [-add] pbf_url"; exit 1;
 fi
  
-#C_MEM is the sum of free memory and cached memory
-C_MEM=$(free -m | grep -i 'mem:' | sed 's/[ \t]\+/ /g' | cut -f4,7 -d' ' | tr ' ' '+' | bc)
 NP=$(grep -c 'model name' /proc/cpuinfo)
-osm2pgsql_OPTS="--slim -d ${OSM_DB} -C ${C_MEM} --number-processes ${NP}"
+osm2pgsql_OPTS="--slim -d ${OSM_DB} --number-processes ${NP}"
 PG_VER=$(pg_config | grep '^VERSION' | cut -f4 -d' ' | cut -f1,2 -d.)
  
 #Clear renderd cache
@@ -68,7 +63,8 @@ ALTER TABLE spatial_ref_sys OWNER TO ${OSM_USER};
 EOF_CMD
 fi
  
-sudo -u ${OSM_USER} osm2pgsql ${osm2pgsql_OPTS} ${PBF_FILE}
+let C_MEM=$(free -m | grep -i 'mem:' | sed 's/[ \t]\+/ /g' | cut -f4,7 -d' ' | tr ' ' '+')-200
+sudo -u ${OSM_USER} osm2pgsql ${osm2pgsql_OPTS} -C ${C_MEM} ${PBF_FILE}
 if [ $? -eq 0 ]; then	#If import went good
 	rm -rf ${PBF_FILE}
 fi
@@ -83,3 +79,39 @@ sed -i.save 's/local all all.*/local all all trust/'	/etc/postgresql/${PG_VER}/m
 service postgresql restart
 service apache2 reload
 service renderd start
+ 
+#update osmosis baseURL in configuration file
+WORKDIR_OSM=/home/${OSM_USER}/.osmosis
+if [ -f ${WORKDIR_OSM}/configuration.txt ]; then
+	#Get the URL from http://download.geofabrik.de/europe/germany.html
+	#example PBF_URL='http://download.geofabrik.de/europe/germany-latest.osm.pbf'
+	UPDATE_URL="$(echo ${PBF_URL} | sed 's/latest.osm.pbf/updates/')"
+	sed -i.save "s|#\?baseUrl=.*|baseUrl=${UPDATE_URL}|" ${WORKDIR_OSM}/configuration.txt
+fi
+ 
+#Update leaflet
+LOC_NAME=$(echo ${PBF_URL##*/} | sed 's/\(.*\)-latest.*/\1/')
+cat >/tmp/latlong.php <<EOF
+<?php
+  \$Address = urlencode(\$argv[1]);
+  \$request_url = "http://maps.googleapis.com/maps/api/geocode/xml?address=".\$Address."&sensor=true";
+  \$xml = simplexml_load_file(\$request_url) or die("url not loading");
+  \$status = \$xml->status;
+  if (\$status=="OK") {
+      \$Lat = \$xml->result->geometry->location->lat;
+      \$Lon = \$xml->result->geometry->location->lng;
+      \$LatLng = "\$Lat,\$Lon";
+	echo "\$LatLng";
+  }
+?>
+EOF
+echo "Updating lat,long for ${LOC_NAME} in Leaflet..."
+LOC_LATLONG=$(php /tmp/latlong.php "${LOC_NAME}")
+if [ -z "${LOC_LATLONG}" ]; then
+	echo "Error: Lat/Long for ${LOC_NAME} not found";
+	echo "Update manually in /var/www/html/leaflet-example.html"
+else
+	echo "Lat/Long for ${LOC_NAME} set to ${LOC_LATLONG}"
+	sed -i.save "s/\.setView(\[[0-9]\+\.[0-9]\+,[ \t]*-\?[0-9]\+\.[0-9]\+/.setView([${LOC_LATLONG}/" /var/www/html/leaflet-example.html
+	sed -i.save "s/L\.marker(\[[0-9]\+\.[0-9]\+,[ \t]*-\?[0-9]\+\.[0-9]\+/L.marker([${LOC_LATLONG}/" /var/www/html/leaflet-example.html
+fi
